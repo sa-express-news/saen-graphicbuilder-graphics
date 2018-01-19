62bbfcc858b7d5c85e31cdf28bccd827e8d2dc41
lukewhyte <lukeawhyte@gmail.com>
2018-01-19 10:58:13 -0600

    consolidated files, ready to deploy

---
+++ border-apprehension-monthly-trends/apprehensions-mom/app/scripts/stable.js
@@ -0,0 +1,29 @@
+/* global pym, buildLineChart */
+
+(function() {
+  'use strict';
+
+  var pymChild;
+
+  function sendHeight () {
+    if (pymChild) {
+      pymChild.sendHeight();
+    }
+  }
+
+  function render() {
+
+    d3.selectAll('svg').remove();
+
+    var chart = buildLineChart('#line-chart', 'assets/yoy-data.json', 'displayStable', sendHeight);
+
+  }
+
+  function load() {
+    pymChild = new pym.Child({
+      renderCallback: render
+    });
+  }
+
+  window.onload = load;
+})();

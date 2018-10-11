e55b142f105398d68d9776378cbdc4eeaca63299
Luke Whyte <lukeawhyte@gmail.com>
2018-10-11 16:52:57 -0500

    finished graphic, modified deploy scripts for graphicBuilder

--- undocumented-resident-percentages/vue.config.js
+++ undocumented-resident-percentages/vue.config.js
@@ -1,9 +1,11 @@
-module.exports = {
-  chainWebpack: (config) => {
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const path  = require('path');
+
+const isProduction = () => process.env.NODE_ENV === 'production';
+
+const setSvgRule = config => {
     const svgRule = config.module.rule('svg');
-
     svgRule.uses.clear();
-
     svgRule
         .use('vue-svg-loader')
         .loader('vue-svg-loader')
@@ -18,5 +20,28 @@ module.exports = {
                 }
             });
         });
-  },
+    return svgRule;
+};
+
+const setGzipRule = config => {
+    const gzipRule = config.plugin('gzip');
+    gzipRule.use(new CompressionWebpackPlugin({
+        filename: '[file]',
+        algorithm: 'gzip',
+        test: /\.js$|\.css$|\.html$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$/,
+        threshold: 10240,
+        minRatio: 0.8,
+    }));
+    return gzipRule;
+};
+
+module.exports = {
+    chainWebpack: config => {
+        const svgRule = setSvgRule(config);
+
+        if (isProduction()) {
+            const gzipRule = setGzipRule(config);
+        }
+    },
+    baseUrl: './',
 };

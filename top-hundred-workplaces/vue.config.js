aaf9eec77441ae91ec54c1235894dc2eaf5f0f27
Luke Whyte <lukeawhyte@gmail.com>
2018-10-16 17:13:55 -0500

    top hundred table underway

---
+++ top-hundred-workplaces/vue.config.js
@@ -0,0 +1,45 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const path  = require('path');
+
+const isProduction = () => process.env.NODE_ENV === 'production';
+
+const setSvgRule = config => {
+    const svgRule = config.module.rule('svg');
+    svgRule.uses.clear();
+    svgRule
+        .use('vue-svg-loader')
+        .loader('vue-svg-loader')
+        .tap(options => {
+            return Object.assign({}, options, { 
+                svgo: {
+                    plugins: [
+                        {removeDoctype: true},
+                        {removeComments: true},
+                        {cleanupIDs: false}
+                    ]
+                }
+            });
+        });
+    return svgRule;
+};
+
+const setGzipRule = config => {
+    const gzipRule = config.plugin('gzip');
+    gzipRule.use(new CompressionWebpackPlugin({
+        filename: '[file]',
+        algorithm: 'gzip',
+        test: /\.js$|\.css$|\.html$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$/,
+    }));
+    return gzipRule;
+};
+
+module.exports = {
+    chainWebpack: config => {
+        const svgRule = setSvgRule(config);
+
+        if (isProduction()) {
+            const gzipRule = setGzipRule(config);
+        }
+    },
+    baseUrl: './',
+};
